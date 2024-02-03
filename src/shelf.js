fac2591379fbf04838938a2e37bcf9ec5a0db5cc
jordan williamson <williamsonjordan05@gmail.com>
2024-02-03 10:51:46 -0700

    shelf finished

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
+function shelfBook(book, sciFiShelf){
+  if (sciFiShelf.length < 3) {
+    sciFiShelf.unshift(book);
+  }
+}
+
+function unshelfBook(bookTitle, shelf){
+  var remove = shelf.findIndex(book => book.title === bookTitle)
+  if (remove !== -1){
+    var removeBook = shelf.splice(remove, 1)[0];
+    return removeBook;
+  }
+}
+
+function listTitles(titles){
+  var list = titles.map(book => book.title).join(', ');
+  return list;
+}
+
+function searchShelf(title, shelf) {
+  if (Array.isArray(shelf)) {
+    return shelf.some(book => book.title === title);
+  } else {
+    return false;
+  }
+}
+
+
+
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
+
+
